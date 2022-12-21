ba20d09cd1212f202f249b93d1b08018a08b9dac
Colin Wong <cwongmath@gmail.com>
2022-12-20 23:23:08 -0600

    we do some cheeky equations (d21 p2)

--- 2022day21.py
+++ 2022day21.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from lib import *
 
 year, day = 2022, 21
@@ -9,9 +10,13 @@ lines = puzzle_input.splitlines()
 
 @dataclass
 class Operation:
-    a: str
-    b: str
+    a: str|Operation
+    b: str|Operation
     op: str
+    humn: bool
+
+    def __str__(self) -> str:
+        return f'({str(self.a)}){self.op}({str(self.b)})'
 
 monkeys: Dict[str, int|Operation] = dict()
 
@@ -22,14 +27,21 @@ for line in lines:
             monkeys[m.groups()[0]] = int(m.groups()[1])
         else:
             m = re.match(r"(\w{4}): (\w{4}) (.) (\w{4})", line)
-            monkeys[m.groups()[0]] = Operation(m.groups()[1], m.groups()[3], m.groups()[2])
+            monkeys[m.groups()[0]] = Operation(m.groups()[1], m.groups()[3], m.groups()[2], False)
 
 def find_value(t: str):
+    if t == 'humn':
+        return 'humn'
     monkey = monkeys[t]
     if isinstance(monkey, int):
         return monkey
     va = find_value(monkey.a)
     vb = find_value(monkey.b)
+    if va == 'humn' or vb == 'humn':
+        return Operation(va, vb, monkey.op, humn=True)
+    if isinstance(va, Operation) or isinstance(vb, Operation):
+        return Operation(va, vb, monkey.op, humn=True)
+
     match monkey.op:
         case '+':
             return va+vb
@@ -40,4 +52,5 @@ def find_value(t: str):
         case '/':
             return va//vb
 
-ans(find_value('root'))
+print(find_value('root'))
+# ans(find_value('root'))
