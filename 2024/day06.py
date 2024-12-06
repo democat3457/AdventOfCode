5605741c182f312fef2289b76b63eda7ec3bc884
democat <cwongmath@gmail.com>
2024-12-06 00:17:17 -0600

    2024 day 6, but its not pretty

--- 2024/day06.py
+++ 2024/day06.py
@@ -19,19 +19,74 @@ lines = puzzle_input.splitlines()
 
 grid = Grid(lines)
 
-total = 0
+def right_rotate(c):
+    return Coor(c.y, -c.x)
 
-visited = set()
 for c in grid:
     if grid[c] == '^':
-        guard_pos = c
+        start_guard_pos = c
         break
-rot = Coor(-1, 0)
-while guard_pos in grid:
-    # print(guard_pos)
-    visited.add(guard_pos.tup)
-    while (guard_pos + rot) in grid and grid[guard_pos + rot] == '#':
-        rot = Coor(rot.y, -rot.x)
-    guard_pos = guard_pos + rot
 
-ans(len(visited))
+# Part 01
+# total = 0
+# visited = set()
+# guard_pos = start_guard_pos
+# rot = Coor(-1, 0)
+# while guard_pos in grid:
+#     # print(guard_pos)
+#     visited.add(guard_pos.tup)
+#     while (guard_pos + rot) in grid and grid[guard_pos + rot] == "#":
+#         rot = right_rotate(rot)
+#     guard_pos = guard_pos + rot
+# ans(len(visited))
+# quit()
+
+# Part 02 Smart? no worky
+# possible = set()
+# visited = set()
+# visited_rots = defaultdict(set)
+# guard_pos = start_guard_pos
+# rot = Coor(-1, 0)
+# while guard_pos in grid:
+#     # print(guard_pos)
+#     if guard_pos.tup in visited_rots:
+#         # print(f"intersect at {guard_pos}; cur rot {rot}; visited {visited[guard_pos.tup]}")
+#         if right_rotate(rot).tup in visited_rots[guard_pos.tup]:
+#             if (guard_pos + rot).tup not in visited:
+#                 possible.add((guard_pos + rot).tup)
+#     visited.add(guard_pos.tup)
+#     visited_rots[guard_pos.tup].add(rot.tup)
+#     temp_pos = guard_pos
+#     while (temp_pos - rot) in grid and grid[temp_pos - rot] != '#':
+#         temp_pos = temp_pos - rot
+#         if rot.tup in visited_rots[temp_pos.tup]:
+#             break
+#         visited_rots[temp_pos.tup].add(rot.tup)
+#     turned = False
+#     while (guard_pos + rot) in grid and grid[guard_pos + rot] == '#':
+#         rot = right_rotate(rot)
+#         if turned: print("hi") # never runs, so there is no corner case
+#         turned = True
+#     guard_pos = guard_pos + rot
+# ans(len(possible))
+# quit()
+
+# Part 02 Bruteforce
+total = 0
+for c in tqdm(grid, total=grid.width * grid.height):
+    if grid[c] == '.':
+        guard_pos = start_guard_pos
+        funny_grid = grid.copy()
+        funny_grid[c] = '#'
+        visited = set()
+        rot = Coor(-1, 0)
+        while guard_pos in funny_grid:
+            # print(guard_pos)
+            if (guard_pos.tup, rot.tup) in visited:
+                total += 1
+                break
+            visited.add((guard_pos.tup, rot.tup))
+            while (guard_pos + rot) in funny_grid and funny_grid[guard_pos + rot] == "#":
+                rot = right_rotate(rot)
+            guard_pos = guard_pos + rot
+ans(total)
